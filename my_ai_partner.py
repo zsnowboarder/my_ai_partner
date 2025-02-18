5adc5becf67744f4a547db12467c9edf57aa24a1
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2025-02-17 19:02:57 -0800

    Update my_ai_partner.py

--- my_ai_partner.py
+++ my_ai_partner.py
@@ -346,7 +346,7 @@ with col8:
     if st.button("Poetic", help="Get Your Shakespeare On"):
         placeholder = st.empty()
         placeholder.write("Please be patient as it may take me a minute or two to generate a response with this free version........")
-        result = generate("Improve the writing with a poetic tone. Explain the result.", new_data)
+        result = generate("Improve the writing with a poetic tone while making it light-hearted. Explain the result.", new_data)
         placeholder.empty()
         #placeholder.write("With this proof of concept, it is possible to use AI to reduce the repetive tasks and put officers back on the road. I can help add entities and text pages using details extracted from the officer's narrative. The possibilities are endless.")
         #st.text_area("Response", result, height=800)
